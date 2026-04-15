a2dd602fd4bce1a4192cd2fc53f5a76cf8e3451a
黄金庆(26522268) <huangjinqing001@ke.com>
2026-04-15 13:11:46 +0800

    feat: configure Django admin with Summernote editor

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Tag, Post
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'slug')
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+    list_display = ('title', 'is_draft', 'created_at', 'updated_at')
+    list_filter = ('is_draft', 'tags')
+    search_fields = ('title',)
+    filter_horizontal = ('tags',)
+    list_editable = ('is_draft',)
