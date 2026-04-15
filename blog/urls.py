2c0230cb105feda99fcb3d7490d7493d317e5c02
黄金庆(26522268) <huangjinqing001@ke.com>
2026-04-15 13:17:02 +0800

    feat: add all views, URL config, and index template

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('posts/', views.PostListView.as_view(), name='post_list'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('tag/<slug:slug>/', views.TagPostsView.as_view(), name='tag_posts'),
+    path('search/', views.SearchView.as_view(), name='search'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
+from django.db.models import Q
+from .models import Post, Tag
 
-# Create your views here.
+
+class IndexView(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Post.objects.filter(is_draft=False)
+
+
+class PostListView(ListView):
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Post.objects.filter(is_draft=False)
+
+
+class PostDetailView(DetailView):
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+    def get_queryset(self):
+        return Post.objects.filter(is_draft=False)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.object
+        context['prev_post'] = (
+            Post.objects.filter(is_draft=False, created_at__lt=post.created_at)
+            .order_by('-created_at')
+            .first()
+        )
+        context['next_post'] = (
+            Post.objects.filter(is_draft=False, created_at__gt=post.created_at)
+            .order_by('created_at')
+            .first()
+        )
+        return context
+
+
+class TagPostsView(ListView):
+    template_name = 'blog/tag_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        self.tag = get_object_or_404(Tag, slug=self.kwargs['slug'])
+        return Post.objects.filter(tags=self.tag, is_draft=False)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = self.tag
+        return context
+
+
+class SearchView(ListView):
+    template_name = 'blog/search.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        self.query = self.request.GET.get('q', '').strip()
+        if not self.query:
+            return Post.objects.none()
+        return Post.objects.filter(
+            Q(title__icontains=self.query) | Q(content__icontains=self.query),
+            is_draft=False,
+        ).distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.query
+        return context
